8bf60f4484239077a36ce091418b580bc3c88e25
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-04-10 21:50:50 -0300

    done

--- lib/elxr_yt_wb_scrpr.ex
+++ lib/elxr_yt_wb_scrpr.ex
@@ -1,18 +1,16 @@
 defmodule ElxrYtWbScrpr do
-  @moduledoc """
-  Documentation for `ElxrYtWbScrpr`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElxrYtWbScrpr.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  use Hound.Helpers
+  def last_video_from() do
+    IO.puts "starting"
+    Hound.start_session
+    navigate_to "https://www.youtube.com/channel/UC0l2QTnO1P2iph-86HHilMQ/videos"
+    Process.sleep(5000)
+    last_video = page_source()
+                  |> Floki.find("#video-title")
+                  |> Floki.attribute("href")
+                  |> Enum.at(0)    
+    navigate_to "https://www.youtube.com/#{last_video}"
+    Process.sleep(10000)
+    Hound.end_session
   end
 end
